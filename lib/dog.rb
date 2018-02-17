df63c90bdd76d1b3b10ae9938e7f4ff993667f9b
Harleigh Abel <harleighabel@gmail.com>
2018-02-17 05:44:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 class Dog 
+<<<<<<< HEAD
   attr_accessor :name 
+=======
+  attr_accessor :name
+>>>>>>> e57fb53dbb3652b8179b1dcaf850f92dafddb06b
   @@all = []
   
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     @@all << self
+=======
+    @@all << name
+>>>>>>> e57fb53dbb3652b8179b1dcaf850f92dafddb06b
   end 
   
   def self.clear_all
@@ -12,7 +20,12 @@ class Dog
   end 
   
   def self.all
+<<<<<<< HEAD
     @@all.each {|dog| puts dog.name}
   end  
   
+=======
+    @@all
+  end  
+>>>>>>> e57fb53dbb3652b8179b1dcaf850f92dafddb06b
 end  
